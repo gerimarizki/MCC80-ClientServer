da4c765c4dd70a0d905483cda17f5d6a80eeb355
gerimarizki <gerimarizki84@gmail.com>
2023-07-20 12:49:43 +0700

    University CRUD

--- API/Program.cs
+++ API/Program.cs
@@ -1,15 +1,23 @@
+using API.Contracts;
 using API.Data;
+using API.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+// Add DbContext to the container
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<BookingDbContext>(options => options.UseSqlServer(connectionString));
 
-
+// Add services to the container.
 builder.Services.AddControllers();
+
+// Add repositories to the container.
+builder.Services.AddScoped<IUniversityRepository, UniversityRepository>();
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
