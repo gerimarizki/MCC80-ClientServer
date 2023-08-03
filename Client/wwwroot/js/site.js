f90378500210c36284262dd6f87718497a35febb
gerimarizki <gerimarizki84@gmail.com>
2023-08-03 15:00:12 +0700

    Latihan Grid

---
+++ Client/wwwroot/js/site.js
@@ -0,0 +1,34 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function ChangeColor() {
+    let tes1 = document.getElementById("tes1")
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16)
+    tes1.style.backgroundColor = "#" + randomColor;
+
+}
+
+
+function ChangeTextAndFont() {
+    let fonts = ['Arial', 'Verdana', 'Helvetica', 'Times New Roman', 'Courier New'];
+    let tes2 = document.getElementById('tes2');
+    let randomIndex = Math.floor(Math.random() * fonts.length);
+    tes2.style.fontFamily = fonts[randomIndex];
+    tes2.textContent = "Latihan Ganti Text!!!!";
+    tes2.style.fontSize = "30px";
+}
+
+function ChangeBackgroundColorAndText() {
+    let fonts = ['Arial', 'Verdana', 'Helvetica', 'Times New Roman', 'Courier New'];
+    let tes3 = document.getElementById('tes3');
+    tes3.style.backgroundColor = "aqua";
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    let randomIndex = Math.floor(Math.random() * fonts.length);
+    tes3.style.fontFamily = fonts[randomIndex];
+    tes3.style.backgroundColor = "#" + randomColor; 
+    tes3.textContent = "Background and text";
+    tes3.style.fontSize = "30px";
+
+}
