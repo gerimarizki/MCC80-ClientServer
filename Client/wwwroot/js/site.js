1303184d006f21d9aa368674be1ba4ddc79e4087
gerimarizki <gerimarizki84@gmail.com>
2023-08-03 20:36:14 +0700

    Latihan Array

--- Client/wwwroot/js/site.js
+++ Client/wwwroot/js/site.js
@@ -30,5 +30,36 @@ function ChangeBackgroundColorAndText() {
     tes3.style.backgroundColor = "#" + randomColor; 
     tes3.textContent = "MCC 80 MCC 80 MCC 80";
     tes3.style.fontSize = "30px";
-
 }
+
+const animals = [
+    { name: "dory", species: "fish", class: { name: "vertebrata" } },
+    { name: "tom", species: "cat", class: { name: "mamalia" } },
+    { name: "nemo", species: "fish", class: { name: "vertebrata" } },
+    { name: "umar", species: "cat", class: { name: "mamalia" } },
+    { name: "gary", species: "fish", class: { name: "human" } },
+]
+
+//bikin sebuah looping ke animals, 2 fungsi :
+//fungsi 1: jika species nya 'cat' maka ambil lalu pindahkan ke variabel OnlyCat
+
+let cat = [];
+for (let i = 0; i < animals.length; i++) {
+    if (animals[i].class.name == "cat") {
+        cat.push(animals[i]);
+    }
+}
+
+//fungsi 2: jika species nya 'fish' maka ganti class -> menjadi 'non-mamalia'
+
+function changeFishClass(animals) {
+    for (let i = 0; i < animals.length; i++) {
+        if (animals[i].species === 'fish') {
+            animals[i].class.name = 'non-mamalia';
+        }
+    }
+}
+
+console.log(cat);
+changeFishClass(animals);
+console.log(animals);
